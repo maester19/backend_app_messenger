820e699ad075bc46ad353f7cde2b388abb5dd626
maesterlyb <yves.ekanga27@gmail.com>
2023-01-05 19:07:09 +0100

    [feat]: create Repertoire and statut model

---
+++ models/Statut.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+const repertoireSchema = mongoose.Schema({
+  _id: { type: String, required: true },
+  userId: { type: String, ref: "User", required: true },
+  color: { type: String, required: true },
+  content: { type: String, required: true },
+  mediaUrl: { type: String, required: true },
+  viewList: [{ type: String, required: true }],
+  createdAt: { type: Date, required: true },
+}, { _id: false });
+
+module.exports = mongoose.model('Repertoire', repertoireSchema); 
